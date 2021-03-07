e96d221fc394a7e9fde5d0a2f2af11a95b4b3f46
Lev Guzman <lguzm038@uottawa.ca>
2021-03-06 21:09:45 -0500

    Initialized Files

---
+++ data_uploader.py
@@ -0,0 +1,9 @@
+import psycopg2
+
+
+def connect_to_group_db():
+    pass
+
+def upload_data():
+    connect_to_group_db()
+
---
+++ special_measures_dim_filter.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from data_uploader import upload_data 
+
+def special_measures_dim_filter():
+    pass
+
+special_measures_dim_filter()
