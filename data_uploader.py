86a374f930bddc949b30ca40593427a63f05d8ae
Lev <lguzm038@uottawa.ca>
2021-03-07 14:10:42 -0500

    .

--- data_uploader.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-
-def connect_to_group_db():
-    pass
-
-def upload_data():
-    connect_to_group_db()
-
--- special_measures_dim_filter.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-from data_uploader import upload_data 
-
-def special_measures_dim_filter():
-    pass
-
-special_measures_dim_filter()
