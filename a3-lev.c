f5aace11e9d916127eaf9b7766911e0f7a5d253a
Lev <lguzm038@uottawa.ca>
2021-03-16 17:18:50 -0400

    Some changes

--- a3-lev.c
+++ a3-lev.c
@@ -44,15 +44,56 @@ TA office hours:
 
 pthread_t ta; //var to hold the TA Thread
 pthread_mutex_t lock; //var to hold the mutex 
-sem_t s1,s2,s3;//semaphore
+sem_t s1,s2,s3,s4;//semaphore
 int In = 0;
 int Out = 0;
-pthread_t student_buffer[3]; //communication buffer
+int student_buffer[3]; //communication buffer
+
+
+void help(int student){
+
+    printf("I'm helping student %d\n", student);
+
+    int random_time = rand() % (50 + 1 - 1) + 1;
+    
+    for(int i = 0; i < random_time; i++){
+    }
+
+}
+
+void append(int student_id){
+
+    student_buffer[In]  = student_id;
+
+    In = (In + 1) % 3;
+}
+
+int take(){
+
+    int next = student_buffer[Out];
+
+    Out = (Out + 1) % 3;
+
+    return next;
+}
 
 
 void* student(void* args){
 
     int student_id = *((int *) args);
+
+   // printf("%d\n", student_id);
+
+
+    sem_post(&s4);
+
+    //Buffer definition
+    //Producer
+    sem_wait(&s3); 
+    sem_wait(&s1);
+    append(student_id);
+    sem_post(&s1);
+    sem_post(&s2);
 
     return NULL;
 }
@@ -61,7 +102,30 @@ void* teaching_assistant(void* args){
 
     int ta_id = *((int *) args);
 
+    int student_to_help;
+    int semaphore4_value;
+    sleep(4);
+    sem_getvalue(&s4, &semaphore4_value);
+
     
+    printf("%d", semaphore4_value);
+
+   /* while(semaphore4_value != 1){
+
+        //busy work 
+        printf("I am asleep");
+        sem_getvalue(&s4, &semaphore4_value);
+    }*/
+
+    //Buffer definirion
+    //consumer
+    sem_wait(&s2);
+    sem_wait(&s1);
+    student_to_help = take();
+    sem_post(&s1);
+    sem_post(&s3);
+    //consume
+    help(student_to_help);
 
     return NULL;
 }
@@ -89,16 +153,17 @@ int main(int argc, char const *argv[]){
             sem_init(&s1,0,1); //init semaphore to value of 1, number of processes accessing the buffer
             sem_init(&s2, 0, 0); //Number of full slots, 0 at the beginning
             sem_init(&s3, 0, 3); //since queue is of size 3, empty slots
+            sem_init(&s4, 0, 0); //0 means ta is asleep, 1 that is awake
             pthread_create(&ta, NULL, &teaching_assistant, &ta_id); //id will always be -1
-            pthread_join(ta, NULL );
 
-            /*pthread_t thread_id[n_students]; // n student threads
+            pthread_t thread_id[n_students]; // n student threads
             for (int i = 0; i < n_students; i++){ //creates n student threads
                 //printf("%d", i);
                 pthread_create(&thread_id[i], NULL, &student , &i); //the i will uniquely identify each student
                 pthread_join(thread_id[i], NULL);
-            }*/
+            }
 
+            pthread_join(ta, NULL );
             pthread_mutex_destroy(&lock); //good practice to destroy mutex when done
             sem_destroy(&s1);
             sem_destroy(&s2);
